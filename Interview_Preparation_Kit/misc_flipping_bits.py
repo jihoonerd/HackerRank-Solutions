203704e215f3805e17e2f997c06f9e67f6a903e7
Jihoon Kim <jihoonerd@outlook.com>
2022-11-06 11:45:25 +0900

    misc flip bits

---
+++ Interview_Preparation_Kit/misc_flipping_bits.py
@@ -0,0 +1,45 @@
+#!/bin/python3
+
+import math
+import os
+import random
+import re
+import sys
+
+#
+# Complete the 'flippingBits' function below.
+#
+# The function is expected to return a LONG_INTEGER.
+# The function accepts LONG_INTEGER n as parameter.
+#
+
+
+def flippingBits(n):
+    # Write your code here
+    max_bits = 32
+
+    binary_n = bin(n)[2:]
+    binary_n_ext = "0" * (max_bits - len(binary_n)) + binary_n
+    flipped = ""
+
+    for bit in binary_n_ext:
+        if bit == "0":
+            flipped += "1"
+        else:
+            flipped += "0"
+    return int(flipped, 2)
+
+
+if __name__ == "__main__":
+    fptr = open(os.environ["OUTPUT_PATH"], "w")
+
+    q = int(input().strip())
+
+    for q_itr in range(q):
+        n = int(input().strip())
+
+        result = flippingBits(n)
+
+        fptr.write(str(result) + "\n")
+
+    fptr.close()
